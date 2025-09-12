13f32ee650cca2f58d85d46dd0bbb6b054259a8e
Felix Wu <felixwu@Felixs-MBP.lan>
2025-09-11 22:15:29 -0600

    commit by me

--- code/app/src/main/java/com/example/listycity/MainActivity.java
+++ code/app/src/main/java/com/example/listycity/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.listycity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     ListView cityList;
     ArrayList<String> dataList;
     ArrayAdapter<String> cityAdapter;
+    boolean deleteMode = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +30,50 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         cityList = findViewById(R.id.list_view);
-        String []cities = {"Edmonton", "Vancouver" , "Moscow", "Sydney", "Berlin", "Vienna", "Tokyo", "Beijing" , "Osaka" , "New Delhi"};
+        String []cities = {"Edmonton", "Vancouver"};
 
         dataList = new ArrayList<>();
         dataList.addAll(Arrays.asList(cities));
         cityAdapter = new ArrayAdapter<>( this, R.layout.content, dataList);
         cityList.setAdapter(cityAdapter);
 
+        cityList.setOnItemClickListener((parent, view, position, id) -> {
+            if (deleteMode) {
+                dataList.remove(position);
+                cityAdapter.notifyDataSetChanged();
+                deleteMode = false; // turn off delete mode
+            }
+        });
+
+
+
     }
+
+    public void handleText(View v) {
+        EditText textField = findViewById(R.id.textField);
+        textField.setVisibility(View.VISIBLE);
+        textField.setText("");
+        TextView confirmButton = findViewById(R.id.confirmButton);
+        confirmButton.setVisibility(View.VISIBLE);
+
+
+    }
+
+    public void handleConfirm(View v) {
+        EditText textField = findViewById(R.id.textField);
+        String text = textField.getText().toString();
+        dataList.add(text);
+        cityAdapter.notifyDataSetChanged();
+        textField.setVisibility(View.GONE);
+        TextView confirmButton = findViewById(R.id.confirmButton);
+        confirmButton.setVisibility(View.GONE);
+
+    }
+
+    public void handleDelete(View v) {
+        if (!dataList.isEmpty()) {
+            deleteMode = true;
+        }
+    }
+
 }
